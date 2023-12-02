ea32456d951580669df9c3b83cfd73904dace491
Loyd <us.flores.loyd@gmail.com>
2023-12-02 17:52:15 -0500

    Basic Styling, Chatbot

--- VII.Implementation/ssrc-model/src/components/Navbar.jsx
+++ VII.Implementation/ssrc-model/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
+import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+import logo from './d2g_logo.png'; // Import the logo image
 
 function Navbar() {
   return (
-    <div>
-      <NavLink to="/"> Home </NavLink>
-      <NavLink to="/recommend"> Recommend </NavLink>
-      <NavLink to="/chat"> Chat </NavLink>
+    <div className="navbar">
+      <img src={logo} alt="Logo" className="logo" /> {/* Use imported logo */}
+      <NavLink to="/" exact className="nav-link" activeClassName="nav-link-selected"> Home </NavLink>
+      <NavLink to="/recommend" className="nav-link" activeClassName="nav-link-selected"> Recommend </NavLink>
+      <NavLink to="/chat" className="nav-link" activeClassName="nav-link-selected"> Chat </NavLink>
     </div>
   );
 }
